e441bcf1af658b0afb0af3ab6acf5ae16481cb6e
pipopotamasu <ec0156hx@gmail.com>
2024-10-30 21:15:33 -0400

    spa mode

---
+++ remix-spa/app/root.tsx
@@ -0,0 +1,135 @@
+import { useEffect } from "react";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Form,
+  Outlet,
+  Links,
+  Meta,
+  NavLink,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useNavigate,
+  useSubmit,
+} from "@remix-run/react";
+import appStylesHref from "./app.css?url";
+
+import { getContacts, createEmptyContact } from "./data";
+
+export const clientLoader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof clientLoader>();
+  const navigation = useNavigation();
+  const navigate = useNavigate();
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
+              <input
+                id="q"
+                className={searching ? "loading" : ""}
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+                defaultValue={q || ""}
+              />
+              <div id="search-spinner" hidden={!searching} aria-hidden />
+            </Form>
+            <Form
+              method="post"
+              onSubmit={async () => {
+                const contact = await createEmptyContact();
+                navigate(`/contacts/${contact.id}/edit`);
+              }}
+            >
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+        <div
+          id="detail"
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+        >
+          <Outlet />
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
