93693429d33d4da71ee9ca2470e09a921e667c86
pipopotamasu <ec0156hx@gmail.com>
2024-11-23 22:54:34 -0500

    update to react router v7

--- remix-emotion/app/routes.ts
+++ remix-emotion/app/routes.ts
@@ -1,7 +1,4 @@
-import {
-  type RouteConfig,
-  index,
-} from "@remix-run/route-config";
+import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [
   index("pages/home.tsx"),
